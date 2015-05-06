87026b68d67422705368c83732b668cae862181b
Alexander Putilin <avp-13@yandex.ru>
2015-05-06 20:37:15 +0300

    Add flask-sqlalchemy/flask-migrate boilerplate

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 Bootstrap(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-if __name__ == '__main__':
+
+@manager.command
+def run():
     app.run(debug=True)
+
+if __name__ == '__main__':
+    manager.run()

c3f8032d51a842d1c84f64ae201fabcd5562f815
Alexander Putilin <avp-13@yandex.ru>
2015-05-13 21:19:16 +0300

    Don't spam terminal with debug messages

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Auto-launching using this: export PROMPT_COMMAND='/Users/putilin/client.py "`fc -nl -1`"'
+# Auto-launching using this: export PROMPT_COMMAND='/Users/putilin/histsync/client.py "`history 1`" >> ~/histsync.log 2>&1'
 import os
 import sys
 
@@ -11,19 +11,32 @@ if os.fork() != 0:
 
 import requests
 import re
-assert len(sys.argv) == 2
+import logging
 
-history_output = sys.argv[1]
-m = re.match(r"[ ]*(\d+)[ ][ ](.*)", history_output)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(process)d - %(name)s - %(levelname)s - %(message)s')
 
-command_id = m.group(1)
-command_text = m.group(2)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
+try:
+    assert len(sys.argv) == 2
 
-USERNAME = "eleweek"
-HOST = "histsync.herokuapp.com"
-API_KEY = "9b946c76-1688-4d3d-9b13-c4d25ef878ef"
-payload = {'api_key': API_KEY, 'command_text': command_text, "id": '{}${}'.format(shell_pid, command_id)}
+    history_output = sys.argv[1]
+    m = re.match(r"[ ]*(\d+)[ ][ ](.*)", history_output)
 
-r = requests.post("http://{}/api/v0/user/{}/add_command".format(HOST, USERNAME), data=payload)
-r.raise_for_status()
+    command_id = m.group(1)
+    command_text = m.group(2)
+
+    USERNAME = "eleweek"
+    HOST = "histsync.herokuapp.com"
+    API_KEY = "5360f714-c0da-4d64-9aa6-1ec95630ddc3"
+    payload = {'api_key': API_KEY, 'command_text': command_text, "id": '{}${}'.format(shell_pid, command_id)}
+
+    r = requests.post("http://{}/api/v0/user/{}/add_command".format(HOST, USERNAME), data=payload)
+    r.raise_for_status()
+except Exception as e:
+    logging.exception(e)

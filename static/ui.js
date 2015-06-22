47ee064a3dda8862b80757f99d88332a3f527ea3
elsie <elimohl@gmail.com>
2015-06-23 02:51:13 +0300

    more formating 'cause i love it

--- static/ui.js
+++ static/ui.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     var html_escape = function(str) {
         return $('<div/>').text(str).html();
     };
@@ -13,24 +13,24 @@ $( document ).ready(function() {
         bootbox.dialog({
             title: title,
             message:
-                    '<div class="row">  ' +
-                        '<div class="col-md-12"> ' +
-                            '<form class="form-horizontal"> ' +
-                                '<div class="form-group"> ' +
-                                    '<label class="col-md-4 control-label" for="pbf-description' + command_id + '">Description</label> ' +
-                                    '<div class="col-md-4"> ' +
-                                        '<input id="pbf-description-' + command_id + '" name="pbf-description-' + command_id + '" type="text" placeholder="Description(can be empty)" value="' + quot_escape(command_description) + '" class="form-control input-md"> ' +
-                                    '</div>' + 
-                                '</div>' + 
-                                '<div class="form-group"> ' +
-                                    '<label class="col-md-4 control-label" for="pbf-command-' + command_id + '">Command</label> ' +
-                                    '<div class="col-md-4"> ' +
-                                        '<input id="pbf-command-' + command_id + '" name="pbf-command-' + command_id + '" type="text" placeholder="Command" class="form-control input-md" value="' + quot_escape(command_text) + '"> ' +
-                                    '</div>' +
-                                '</div>' +
-                            '</form>' + 
+                '<div class="row">  ' +
+                  '<div class="col-md-12"> ' +
+                    '<form class="form-horizontal"> ' +
+                      '<div class="form-group"> ' +
+                        '<label class="col-md-4 control-label" for="pbf-description' + command_id + '">Description</label> ' +
+                        '<div class="col-md-4"> ' +
+                          '<input id="pbf-description-' + command_id + '" name="pbf-description-' + command_id + '" type="text" placeholder="Description(can be empty)" value="' + quot_escape(command_description) + '" class="form-control input-md"> ' +
+                        '</div>' + 
+                      '</div>' + 
+                      '<div class="form-group"> ' +
+                        '<label class="col-md-4 control-label" for="pbf-command-' + command_id + '">Command</label> ' +
+                        '<div class="col-md-4"> ' +
+                          '<input id="pbf-command-' + command_id + '" name="pbf-command-' + command_id + '" type="text" placeholder="Command" class="form-control input-md" value="' + quot_escape(command_text) + '"> ' +
                         '</div>' +
-                    '</div>',
+                      '</div>' +
+                    '</form>' + 
+                  '</div>' +
+                '</div>',
             buttons: {
                 cancel: {
                     label: "Cancel",
@@ -131,10 +131,10 @@ $( document ).ready(function() {
             var command_text = $('#pbf-command-' + command_id).val();
             var command_description = $('#pbf-description-' + command_id).val();
             $.post("/_publish_command/" + command_id,
-                    {
-                        description: command_description,
-                        command: command_text
-                    }
+                   {
+                       description: command_description,
+                       command: command_text
+                   }
                   ).done(function(done){
                 $(that).hide();
                 $(".btn-unpublish-command-" + command_id).show();
@@ -159,10 +159,10 @@ $( document ).ready(function() {
             var command_text = $('#pbf-command-' + command_id).val();
             var command_description = $('#pbf-description-' + command_id).val();
             $.post("/_edit_command/" + command_id,
-                    {
-                        description: command_description,
-                        command: command_text
-                    }
+                   {
+                       description: command_description,
+                       command: command_text
+                   }
                   ).done(function(done){
                     $(".pre-command-" + command_id).html(" $ " + html_escape(command_text));
                     $(".command-description-" + command_id).html(html_escape(command_description));

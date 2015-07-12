679746b8cbaf6be4f8ba04e457a3eff6881846ee
Patrick Allen <prallen90@gmail.com>
2015-07-12 11:12:17 -0400

    Add humanized time-since on shared commands.

--- app.py
+++ app.py
@@ -46,6 +46,36 @@ def url_for_other_page(page):
 app.jinja_env.globals['url_for_other_page'] = url_for_other_page
 
 
+@app.template_filter()
+def timesince(dt, default="just now"):
+    """
+    Jinja filter that takes a datetime and retuns the
+    'human' version of time since said datetime.
+
+    e.g. 28 minutes ago
+    """
+
+    now = datetime.utcnow()
+    diff = now - dt
+    
+    periods = (
+        (diff.days / 365, "year", "years"),
+        (diff.days / 30, "month", "months"),
+        (diff.days / 7, "week", "weeks"),
+        (diff.days, "day", "days"),
+        (diff.seconds / 3600, "hour", "hours"),
+        (diff.seconds / 60, "minute", "minutes"),
+        (diff.seconds, "second", "seconds"),
+    )
+
+    for period, singular, plural in periods:
+        
+        if period:
+            return "%d %s ago" % (period, singular if period == 1 else plural)
+
+    return default
+
+
 @github.access_token_getter
 def token_getter():
     if current_user.is_authenticated():

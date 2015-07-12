7537dfd1c067e087a2704330ed03f91eb32279f7
Patrick Allen <prallen90@gmail.com>
2015-07-12 12:09:20 -0400

    Use humanize instead of custom function

--- app.py
+++ app.py
@@ -57,23 +57,8 @@ def timesince(dt, default="just now"):
 
     now = datetime.utcnow()
     diff = now - dt
-    
-    periods = (
-        (diff.days / 365, "year", "years"),
-        (diff.days / 30, "month", "months"),
-        (diff.days / 7, "week", "weeks"),
-        (diff.days, "day", "days"),
-        (diff.seconds / 3600, "hour", "hours"),
-        (diff.seconds / 60, "minute", "minutes"),
-        (diff.seconds, "second", "seconds"),
-    )
 
-    for period, singular, plural in periods:
-        
-        if period:
-            return "%d %s ago" % (period, singular if period == 1 else plural)
-
-    return default
+    return humanize.naturaltime(diff) 
 
 
 @github.access_token_getter
